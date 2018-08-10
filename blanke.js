dc6c6112ab5288b82b1e13796a9fec3655718092
Xavier Harris <xharris1@abedin.irc.umbc.edu>
2018-08-10 14:50:09 -0400

    added hex string to rgb function

--- blanke.js
+++ blanke.js
@@ -54,6 +54,23 @@ String.prototype.toHex = function() {
     return intToARGB(hashCode(this));
 }
 
+String.prototype.toRgb = function() {
+    let hex = this;
+
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
 String.prototype.replaceAll = function(find, replace) {
     return this.replace(new RegExp(find, 'g'), replace);
 };
